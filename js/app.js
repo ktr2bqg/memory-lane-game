7ca88da1389dde42aca386a76697ce9fd776275f
ktr.krose <ktr.krose@gmail.com>
2024-07-22 22:08:17 -0400

    card-movement

--- js/app.js
+++ js/app.js
@@ -1,4 +1,30 @@
-let board
-let winner
-let cards
+/*------Constants------*/ 
 
+
+
+
+/*------Variables------*/
+let board;
+let winner;
+/*------Cached Element References------*/
+const cardEls = document.querySelectorAll('cards')
+const resetBtn = document.getElementById('reset')
+
+
+/*------Event Listeners------*/
+
+
+
+/*------Functions------*/
+init();
+
+function init() {
+  board = 
+    [ "","","","","",
+    "","","", "","",]
+    turn = "";
+    winner = false;
+    render()
+};
+
+function updateBoard()
